8299a39e1198d0ae3fa337163650676c46bc5de2
unknown <sazwerling@gmail.com>
2022-10-17 15:46:25 -0700

    navbar and cart/drawer function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { NavBar } from "./components/NavBar";
 import ProductPage from "./pages/ProductPage";
+import  Cart  from "./components/Cart"
 
 function App() {
    return (
       <div className="App">
          <Router>
             <NavBar />
+            <Cart />
             <Routes>
                <Route path="/products/:handle" element={<ProductPage />} />
                <Route path="/" element={<Home />} />

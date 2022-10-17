f301f126b725c55f59d0c7ca9a1b20f41bf3bced
unknown <sazwerling@gmail.com>
2022-10-17 13:50:42 -0700

    update to react18, use chakra ui to style list of products

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client'; // New For React18
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import ShopProvider from "./context/shopContext";
+import { ChakraProvider } from '@chakra-ui/react'
 
-ReactDOM.render(
+const rootElement = document.getElementById("root"); // New For React 18
+const root = createRoot(rootElement);
+
+root.render(
    <React.StrictMode>
-      <ShopProvider>
-         <App />
-      </ShopProvider>
+      <ChakraProvider>
+         <ShopProvider>
+            <App />
+         </ShopProvider>
+      </ChakraProvider>
    </React.StrictMode>,
-   document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,37 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect } from "react";
 import { ShopContext } from "../context/shopContext";
-import { Link } from 'react-router-dom'
-
+import { Link } from "react-router-dom";
+import { Box, Grid, Text, Image } from "@chakra-ui/react";
 
 const Home = () => {
-    const { fetchAllProducts, products } = useContext(ShopContext)
+   const { fetchAllProducts, products } = useContext(ShopContext);
 
-    useEffect(() => {
-        fetchAllProducts()
-    }, [fetchAllProducts])
+   useEffect(() => {
+      fetchAllProducts();
+   }, [fetchAllProducts]);
 
-
-
-    if (!products) return <div>loading...</div>
-    console.log(products)
-
-   return(
-    <div>
-       {
-        products.map((product) => {
-           return <Link to={`/products/${product.handle}`} key={product.id} >{product.title}</Link>
-        })
-       }
-    </div>
-   )
-}
+   if (!products) return <div>loading...</div>;
+   return (
+      <Box>
+         <Grid templateColumns="repeat(3, 1fr)">
+            {products.map((product) => {
+               console.log(product.variants[0].price.amount);
+               return (
+                  <Link to={`/products/${product.handle}`} key={product.id}>
+                     <Box _hover={{ opacity: '80%', }} textAlign="center">
+                        <Image
+                           src={product.images[0].src}
+                           alt={product.images[0].altText}
+                        />
+                        <Text>{product.title}</Text>
+                        <Text>{product.variants[0].price.amount}</Text>
+                     </Box>
+                  </Link>
+               );
+            })}
+         </Grid>
+      </Box>
+   );
+};
 
 export default Home;

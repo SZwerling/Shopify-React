b39b459f11dd73e2392beaeb4148da2463e6c9f0
unknown <sazwerling@gmail.com>
2022-10-19 12:44:08 -0700

    finish cart and navigation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import { NavBar } from "./components/NavBar";
 import ProductPage from "./pages/ProductPage";
 import  Cart  from "./components/Cart"
+import NavMenu from "./components/NavMenu";
 
 function App() {
    return (
@@ -10,6 +11,7 @@ function App() {
          <Router>
             <NavBar />
             <Cart />
+            <NavMenu />
             <Routes>
                <Route path="/products/:handle" element={<ProductPage />} />
                <Route path="/" element={<Home />} />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,47 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import { ShopContext } from "../context/shopContext";
-import { Flex, Icon, Image, Text } from '@chakra-ui/react';
-import { MdOutlineMenuBook } from 'react-icons/md'
-import { AiOutlineShoppingCart } from 'react-icons/ai'
+import { Flex, Icon, Image, Text, Box, Badge } from "@chakra-ui/react";
+import { MdOutlineMenuBook } from "react-icons/md";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 export const NavBar = () => {
-    const { openCart, openMenu, checkout } = useContext(ShopContext)
-    return(
-        <Flex borderBottom="0.25pt white solid" backgroundColor="#FFA8E2" flexDir="row" justifyContent="space-between" p="2rem">
-            <Icon cursor="pointer" w={30} h={30} fill="white" as={MdOutlineMenuBook}></Icon>
-            <Image src={"https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Logologo_1.svg?v=1610055540"}/>
-            <Icon cursor="pointer" w={30} h={30} fill="white" as={AiOutlineShoppingCart}
-            onClick={() => openCart()}
+   const { openCart, openMenu, checkout } = useContext(ShopContext);
+   return (
+      <Flex
+         borderBottom="0.25pt white solid"
+         backgroundColor="#FFA8E2"
+         flexDir="row"
+         justifyContent="space-between"
+         alignItems="center"
+         p="2rem"
+      >
+         <Icon
+            cursor="pointer"
+            w={30}
+            h={30}
+            fill="white"
+            as={MdOutlineMenuBook}
+            onClick={() => openMenu()}
+         ></Icon>
+         <Link to="/">
+            <Image
+               src={
+                  "https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Logologo_1.svg?v=1610055540"
+               }
+            />
+         </Link>
+         <Box>
+            <Icon
+               cursor="pointer"
+               w={30}
+               h={30}
+               fill="white"
+               as={AiOutlineShoppingCart}
+               onClick={() => openCart()}
             ></Icon>
-        </Flex>
-    )
-}
+            <Badge backgroundColor="#FF38BD" borderRadius="50%">{checkout.lineItems?.length}</Badge>
+         </Box>
+      </Flex>
+   );
+};
